3d40055ff4542c7d105f9bb6d43a70740775b6e3
Ghox19 <gonzalo.moncada19@gmail.com>
2023-09-11 16:12:04 -0300

    Feat(Flow): Funcion constructora flow

--- Flow_21321047_MoncadaSanchez.rkt
+++ Flow_21321047_MoncadaSanchez.rkt
@@ -1,1 +1,9 @@
 #lang racket
+
+;CONSTRUCTORES
+;descripción: Función que crea un flujo de un chatbot
+;recursión: no
+;dom: name (String)  X Option*
+;rec: flow
+(define (flow name . options)
+  (list name options))
