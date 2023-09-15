85c87ee19409d27d4b0e5e6986653c16fbd9a0b1
Ghox19 <gonzalo.moncada19@gmail.com>
2023-09-15 18:18:48 -0300

    Feat(Chatbot): Modificador chatbot y constructor

--- Chatbot_21321047_MoncadaSanchez.rkt
+++ Chatbot_21321047_MoncadaSanchez.rkt
@@ -8,12 +8,38 @@
 ;dom: chatbotID (int) X name (String) X welcomeMessage (String)  X  flows*
 ;rec: chatbot
 (define (chatbot id name welcomeMessage . flows)
-  (define (elementos-repetidos lst)
-    (cond
-      ((null? lst) #t)
-      ((member (car lst) (cdr lst)) #f) 
-      (else (elementos-repetidos (cdr lst)))))
-  (if (elementos-repetidos (map get-flow-id flows))
+  (define (eliminar-duplicados-flow lista)
+    (cond ((null? lista) '())
+    (else (cons (car lista) (eliminar-duplicados-flow
+      (filter (lambda (x) (not (equal? (get-flow-id x) (get-flow-id (car lista))))) (cdr lista)))))))
+  (if (null? flows)
     (list id name welcomeMessage flows)
-    (display "")
+    (list id name welcomeMessage (eliminar-duplicados-flow flows))
   ))
+
+;SELECTORES
+(define (get-chatbot-id some-chatbot) (car some-chatbot));Extrae el id de un chatbot
+
+(define (get-chatbot-name some-chatbot) (cadr some-chatbot));Extrae el nombre de un chatbot
+
+(define (get-chatbot-welcomeMessage some-chatbot) (caddr some-chatbot));Extrae el mensaje de un chatbot
+
+(define (get-chatbot-flows some-chatbot) (cadddr some-chatbot));Extrae los flows de un chatbot
+
+;MODIFICADOR
+;descripción: Función añadir flujos a un chatbot
+;recursión: si, debido a que se recorre hasta encontrar el ultimo
+;dom: chatbot X flow
+;rec: chatbot
+(define (chatbot-add-flow some-chatbot flow)
+  (define (rec-list-add lst arg)
+    (if (null? lst)
+    (list arg)
+    (cons (car lst) (rec-list-add (cdr lst) arg))))
+  (define (output some-chatbot flow) (list (get-chatbot-id some-chatbot) (get-chatbot-name some-chatbot) (get-chatbot-welcomeMessage some-chatbot) 
+  (rec-list-add (get-chatbot-flows some-chatbot) flow)))
+  (if (null? (get-chatbot-flows some-chatbot))
+      (output some-chatbot flow)
+      (if (member (get-flow-id flow) (map get-flow-id (get-chatbot-flows some-chatbot)))
+          (display "")
+          (output some-chatbot flow))))
--- User_21321047_MoncadaSanchez.rkt
+++ User_21321047_MoncadaSanchez.rkt
@@ -6,13 +6,8 @@
 ;(require "System_21321047_MoncadaSanchez.rkt")
 ;(require "Chat_History_21321047_MoncadaSanchez.rkt")
 
-(define op1 (option  1 "1) Viajar" 2 4 "viajar" "turistear" "conocer") )
-(define op2 (option  2 "2) Estudiar" 4 3 "aprender" "perfeccionarme"))
-
-(define f10 (flow 1 "Flujo1"))
-
-(define f11 (flow-add-option f10 op1))
-(define f12 (flow-add-option f11 op2))
-
-(define cb10 (chatbot 0 "Asistente" "Bienvenido\n¿Qué te gustaría hacer?"))
-(define cb11  (chatbot 0 "Asistente" "Bienvenido\n¿Qué te gustaría hacer?" f12))
+(define op1 (option  1 "1) Viajar" 2 1 "viajar" "turistear" "conocer"))
+(define op2 (option  2 "2) Estudiar" 3 1 "estudiar" "aprender" "perfeccionarme"))
+(define f10 (flow 1 "flujo1" op1 op2 op2 op2 op2 op1)) ;solo añade una ocurrencia de op2
+(define f11 (flow-add-option f10 op1)) ;se intenta añadir opción duplicada
+(define cb0 (chatbot 0 "Inicial" "Bienvenido\n¿Qué te gustaría hacer?" f10 f10 f10 f10))  ;solo añade una ocurrencia de f10
