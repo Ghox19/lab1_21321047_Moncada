a3dd4b4e2725b3f0f404d43958a20251f442d344
Ghox19 <gonzalo.moncada19@gmail.com>
2023-10-04 19:04:32 -0300

    docs(Option): Documentacion de Option

--- Option_21321047_MoncadaSanchez.rkt
+++ Option_21321047_MoncadaSanchez.rkt
@@ -10,13 +10,32 @@
   (list code message ChatbotCodeLink FlowCodeLink Keyword))
 
 ;SELECTORES
+;descripción: Función que selecciona un code de una opcion
+;recursión: no
+;dom: option(list)
+;rec: number
+(define (get-option-code some-option)(car some-option))
 
-(define (get-option-code some-option)(car some-option));Extrae el codigo de una opcion
-
+;descripción: Función que selecciona un mensaje de una opcion
+;recursión: no
+;dom: option(list)
+;rec: string
 (define (get-option-message some-option)(cadr some-option))
 
+;descripción: Función que selecciona un ChatbotCodeLink de una opcion
+;recursión: no
+;dom: option(list)
+;rec: number
 (define (get-option-ChatbotCodeLink some-option)(caddr some-option))
 
+;descripción: Función que selecciona un InitialFlowCodeLink de una opcion
+;recursión: no
+;dom: option(list)
+;rec: number
 (define (get-option-InitialFlowCodeLink some-option)(cadddr some-option))
 
+;descripción: Función que selecciona las keywords de una opcion
+;recursión: no
+;dom: option(list)
+;rec: list
 (define (get-option-keyword some-option)(car (cddddr some-option)))
